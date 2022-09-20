6b238300d9874365d75f802f6ae8d1a9ae829041
김다빈 <suor1058@naver.com>
2022-09-20 17:47:57 +0900

    first commit

---
+++ src/profile_pages/AboutMe.jsx
@@ -0,0 +1,23 @@
+/*
+    애니메이션 작성- npm i framer-motion 모듈 설치
+*/ 
+
+import {motion} from 'framer-motion';
+
+const AboutMe = ()=>{
+    return (
+        /*
+           <motion>으로 컴포넌트를 감싸준다.
+           initial : 마운트 될 때.
+           animate : 마운트가 완료된 직 후.
+           exit : 언마운트(화면에서 사라지는 것) 될 때.
+        */
+        <motion.div initial={{opacity:0 }} animate={{opacity:1}} exit={{opacity:0}}>
+            <section>
+                <img src="./images/aboutme.png" alt="#" />
+            </section>
+        </motion.div>
+    );
+};
+
+export default AboutMe;
